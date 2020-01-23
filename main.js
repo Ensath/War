06e69878576d5ac4f38abc89517dc402feafc68d
Ensath <nfreed4@gmail.com>
2020-01-23 13:58:03 -0800

    Specified create and deal functions for Deck class

--- main.js
+++ main.js
@@ -14,16 +14,20 @@ class Deck
     create(numberOfSuits,  numberOfRanks){
         for(let i = 1; i <= numberOfRanks; i++){
             for(let j = 0; j < numberOfSuits; j++){
-
+                this.deckorder.push(new Card(i));
             }
         }
     }; 
 
     /* Shuffle the deck */ 
-    shuffle(); 
+    shuffle(){
+
+    }; 
 
     /* deal a card from the deck */ 
-    deal(); 
+    deal(){
+        return this.deckorder.pop();
+    }; 
 } 
 
 class War 
