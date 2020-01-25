15a79ac2f4d69b25a98681476326c0403b41ced9
Ensath <nfreed4@gmail.com>
2020-01-25 15:07:15 -0800

    Added code for shuffling deck

--- main.js
+++ main.js
@@ -21,7 +21,21 @@ class Deck
 
     /* Shuffle the deck */ 
     shuffle(){
+        // Shuffle code from CoolAJ86's answer at https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        var currentIndex = deckorder.length, temporaryValue, randomIndex;
 
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = deckorder[currentIndex];
+          deckorder[currentIndex] = deckorder[randomIndex];
+          deckorder[randomIndex] = temporaryValue;
+        }
     }; 
 
     /* deal a card from the deck */ 
