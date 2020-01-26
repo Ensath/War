bb9df1b3fb0255b1bceda7cd2b4ba749326eca21
Ensath <nfreed4@gmail.com>
2020-01-25 21:55:32 -0800

    Adjusted deckOrder variable name capitalization

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ class Card {
 
 class Deck 
 { 
-    deckorder = [];
+    deckOrder = [];
 
     /* Create the deck of cards */ 
     create(numberOfSuits,  numberOfRanks){
         for(let i = 1; i <= numberOfRanks; i++){
             for(let j = 0; j < numberOfSuits; j++){
-                this.deckorder.push(new Card(i, j));
+                this.deckOrder.push(new Card(i, j));
             }
         }
     }; 
@@ -22,7 +22,7 @@ class Deck
     /* Shuffle the deck */ 
     shuffle(){
         // Shuffle code from CoolAJ86's answer at https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-        var currentIndex = deckorder.length, temporaryValue, randomIndex;
+        var currentIndex = deckOrder.length, temporaryValue, randomIndex;
 
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
@@ -32,15 +32,15 @@ class Deck
           currentIndex -= 1;
       
           // And swap it with the current element.
-          temporaryValue = deckorder[currentIndex];
-          deckorder[currentIndex] = deckorder[randomIndex];
-          deckorder[randomIndex] = temporaryValue;
+          temporaryValue = deckOrder[currentIndex];
+          deckOrder[currentIndex] = deckOrder[randomIndex];
+          deckOrder[randomIndex] = temporaryValue;
         }
     }; 
 
     /* deal a card from the deck */ 
     deal(){
-        return this.deckorder.pop();
+        return this.deckOrder.pop();
     }; 
 
     // Divide the cards into the deck into a given number of stacks of equal size
@@ -49,7 +49,7 @@ class Deck
         for(let i = 0; i < numberOfStacks; i++){
             stacks.push(new Deck());
         }
-        while(this.deckorder.length >= numberOfStacks){
+        while(this.deckOrder.length >= numberOfStacks){
             for(let i = 0; i < numberOfStacks; i++){
                 stacks[i].push(deal());
             }
@@ -67,19 +67,19 @@ class War
         wholeDeck.create(numberOfSuits, numberOfRanks);
         wholeDeck.shuffle();
         let playerDecks = wholeDeck.splitIntoStacks(numberOfPlayers);
-        let numberOfCardsInPlayerDecks = playerDecks[0].deckorder.length * numberOfPlayers;
+        let numberOfCardsInPlayerDecks = playerDecks[0].deckOrder.length * numberOfPlayers;
 
         // Play the game until a player has all the cards
         while(true){
             let cardsOnTable = [];
             for(let i = 0; i < numberOfPlayers; i++){
                 // Check if the player has all the cards - if so, end the game and declare them the winner
-                if(playerDecks[i].deckorder.length === numberOfCardsInPlayerDecks){
+                if(playerDecks[i].deckOrder.length === numberOfCardsInPlayerDecks){
                     console.log("Player ", i, " is the winner!");
                     return;
                 }
                 // Check if the player has run out of cards - if so, ignore them
-                if(playerDecks[i].deckorder.length === 0){
+                if(playerDecks[i].deckOrder.length === 0){
                     continue;
                 }
                 // Have the player reveal a card
